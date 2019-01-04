9dbdc4edc417db0112c42d5e2bdbfdf365e082bb
Satish Kamatkar <skamatkar@illumina.com>
2019-01-03 16:09:39 -0800

    Initial import of the project

---
+++ Xylophone/ VIewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 27/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController{
+    
+    var player: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        playSound(fileName: "note1")
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        let tag = sender.tag
+        
+        let fileName = "note\(tag)"
+     
+        playSound(fileName: fileName)
+    }
+    
+    func playSound(fileName: String) {
+        
+        let url = Bundle.main.url(forResource: fileName, withExtension: "wav")!
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            guard let player = player else { return }
+            
+            player.prepareToPlay()
+            player.play()
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+
+}
+
